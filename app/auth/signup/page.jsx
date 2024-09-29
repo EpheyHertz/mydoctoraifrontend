749f4855fa09dab82b0f743432598f747fb96d0b
denzel-source <denzelomondi55@gmail.com>
2024-09-29 19:33:53 +0300

    Update page.jsx

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -25,7 +25,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/apis/signup/', {
+      const response = await fetch('//https://doctorai-cw25.onrender.com/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
